c12afc9c66b8b1a93b80fcf353e1bd447eba9d28
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2025-12-03 17:25:30 +0100

    updated setup

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,28 @@
-from setuptools import setup
-from Cython.Build import cythonize
+from setuptools import setup, find_packages
 
 setup(
-    ext_modules = cythonize("decoder_cy.pyx"),
+    name="two-time-pad-decrypt",
+    version="2.0.0",
+    description="Two-Time Pad Decryption System using Language Models",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "requests>=2.25.1",
+        "beautifulsoup4>=4.9.3",
+        "tqdm>=4.60.0",
+        "numpy>=1.20.0",
+    ],
+    python_requires=">=3.8",
+    author="Radoslaw Wolnik",
+    author_email="radoslaw.m.wolnik@gmail.com",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Security :: Cryptography",
+    ],
 )
